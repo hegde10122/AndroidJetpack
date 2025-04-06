5b47b10be330c475c760d8a49684a0e3a74bc226
hegde10122 <hegde10122@gmail.com>
2025-04-06 16:16:09 +0530

    app created

---
+++ build.gradle.kts
@@ -0,0 +1,8 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+plugins {
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.kotlin.compose) apply false
+}
+
+//65aa76fc3d5f4ad9838f40a37f4027fc
